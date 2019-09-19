44f1fc2df0f6a12772730a9f1e0adc45d8f862b7
Navjeet <navsandhu343@gmail.com>
2019-09-19 11:32:22 -0400

    Total Number of instructers

--- app/src/main/java/com/example/screamitus_android/Infection.java
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -2,4 +2,16 @@ package com.example.screamitus_android;
 
 public class Infection {
 
+    int calculateTotalInfected(int day) {
+        if (day <= 0) return -1;
+        if (day % 2 == 0) {
+            return 0;
+        } else {
+            if (day > 0 && day <= 7) return 5 * day;
+            else {
+                return 35 + (day - 7) * 8;
+            }
+        }
+    }
 }
+
